f66caa00c14a4ca0320b7bfdb415e8a19ede31f3
AlanMestezk <alanmestezk98@gmail.com>
2024-10-10 09:56:35 -0300

    5 - Style Crypto Page

--- src/pages/Details/Components/DetailsApp.tsx
+++ src/pages/Details/Components/DetailsApp.tsx
@@ -59,11 +59,19 @@ export const DetailApp: React.FC = ()=>{
                         );
 
                         let changePercentRaw = data.data.changePercent24Hr;
+
+                        if (!changePercentRaw || changePercentRaw === '' || isNaN(parseFloat(changePercentRaw))) {
+                            console.error(`Invalid changePercent24hr value for ${data.data.name}: ${changePercentRaw}`);
+                            changePercentRaw = '0'; // Define um valor padrão de '0' se inválido
+                        }
+
                         
 
                         if (!changePercentRaw || changePercentRaw === '' || isNaN(parseFloat(changePercentRaw))) {
                             console.error(`Invalid changePercent24hr value for ${data.data.name}: ${changePercentRaw}`);
                             changePercentRaw = '0'; // Define um valor padrão de '0' se inválido
+                            // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+                            changePercentRaw
                         }
 
                         const resultData = {
@@ -73,7 +81,7 @@ export const DetailApp: React.FC = ()=>{
                             formatedPrice : price.format(Number(data.data.priceUsd)),
                             formatedMarket: priceCompact.format(Number(data.data.marketCapUsd)),
                             formatedVolume: priceCompact.format(Number(data.data.volumeUsd24Hr)),
-                        
+                            changePercentRaw
                         }
 
                         setCoin(resultData)
@@ -102,14 +110,37 @@ export const DetailApp: React.FC = ()=>{
 
         <main className={styles.container}>
 
-            <h1>{coin?.name}</h1>
-            <img 
-                src={`https://assets.coincap.io/assets/icons/${coin?.symbol.toLowerCase()}@2x.png`}
-                alt="" 
-            />
-            <h2>{coin?.formatedPrice}</h2>
-            <h2>{coin?.formatedMarket}</h2>
-            <h2>{coin?.formatedVolume}</h2>
+            <section className={styles.sectionTitle}>
+
+                <strong>{coin?.name}</strong>
+
+                <strong>{coin?.symbol}</strong>
+
+            </section>
+
+            <section className={styles.sectionContent}>
+
+
+                <div className={styles.divTitle}>
+
+                    <h1>{coin?.name}</h1>
+                    <img 
+                        src={`https://assets.coincap.io/assets/icons/${coin?.symbol.toLowerCase()}@2x.png`}
+                        alt="" 
+                    />
+                </div>
+
+                <div className={styles.divdetails}>
+
+                    <h2>Preço: {coin?.formatedPrice}</h2>
+                    <h2>Valor mercado: {coin?.formatedMarket}</h2>
+                    <h2>Volume: {coin?.formatedVolume}</h2>
+                    
+
+                </div>
+
+            </section>
+
 
 
         </main>
